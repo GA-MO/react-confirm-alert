efc237a83f84c3e53597c0644a265f1c1000bf29
GA-MO <thawatchai.deartk@gmail.com>
2017-07-02 15:35:55 +0700

    Disable test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 import expect from 'expect';
-import { add } from '../src';
+// import { add } from '../src';
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
-  });
-});
+// describe('add', () => {
+//   it('should add 2 and 2', () => {
+//     expect(add(2, 2)).toBe(4);
+//   });
+// });
