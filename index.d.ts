1dc5be2d965ab376da1977a4e81e42ad9ee6c3e7
Thawatchai Khuansombat <thawatchai.deartk@gmail.com>
2019-02-27 14:41:45 +0700

    Prettier code

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,21 @@
 declare module 'react-confirm-alert' {
-
-    export interface ReactConfirmAlertProps {
-      title?: string;
-      message?: string;
-      buttons?: Array<{
-        label: string;
-        onClick: () => void;
-      }>;
-      childrenElement?: () => React.ReactNode;
-      customUI?: (customUiOptions: { title: string, message: string, onClose: () => void }) => React.ReactNode;
-      willUnmount?: () => void;
-    }
-  
-    export function confirmAlert(options: ReactConfirmAlertProps): void;
-  
-    export default class ReactConfirmAlert extends React.Component<ReactConfirmAlertProps>{
-    }
+  export interface ReactConfirmAlertProps {
+    title?: string
+    message?: string
+    buttons?: Array<{
+      label: string
+      onClick: () => void
+    }>
+    childrenElement?: () => React.ReactNode
+    customUI?: (customUiOptions: {
+    title: string
+    message: string
+    onClose: () => void
+    }) => React.ReactNode
+    willUnmount?: () => void
   }
+
+  export function confirmAlert(options: ReactConfirmAlertProps): void
+
+  export default class ReactConfirmAlert extends React.Component<ReactConfirmAlertProps> {}
+}
