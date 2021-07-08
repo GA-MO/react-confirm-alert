e15a90af1ce509e63873f30d2367bf3d36aad32e
Alireza Rayej <46888483+alirezarayej@users.noreply.github.com>
2021-07-08 17:47:39 +0000

    Better customUI

--- src/index.js
+++ src/index.js
@@ -103,9 +103,10 @@ export default class ReactConfirmAlert extends Component {
         onClick={this.handleClickOverlay}
       >
         <div className='react-confirm-alert'>
-          {customUI ? (
-            this.renderCustomUI()
-          ) : (
+        {customUI ? (
+          this.renderCustomUI()
+        ) : (
+          <div className='react-confirm-alert'>
             <div className='react-confirm-alert-body'>
               {title && <h1>{title}</h1>}
               {message}
@@ -118,8 +119,8 @@ export default class ReactConfirmAlert extends Component {
                 ))}
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     )
   }
